d0edcbc8882d0e49e44f945f61a1e63cd0ec49e7
Robert Erickson <bobericksonjr@gmail.com>
2021-04-22 16:00:10 -0400

    styling and loop

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ function Book(title, author, pages, isRead) {
     this.author = author
     this.pages = pages
     this.isRead = isRead
+    this.dateEntered = function() {
+        const newDate = new Date();
+        const monthName = newDate.toLocaleString("default", { month: "long" });
+        return `${monthName} ${newDate.getDate()}, ${newDate.getFullYear()}`;
+    }
     this.info = function() {
         return `${title} by ${author}, ${pages} pages, ${isRead ? 'read already' : 'not read yet'}.`
     }
@@ -26,18 +31,63 @@ function addBookToLibrary() {
 
 function displayLibrary() {
     for (let i = 0; i < myLibrary.length; i++) {
-        let para = document.createElement('p');
-        let text = myLibrary[i].title;
-        para.textContent = text;
-        let body = document.querySelector('body');
-        body.appendChild(para);
-
+        // create elements and assign classes/src
+        let author = document.createElement('p');
+        author.textContent = myLibrary[i].author;
+        author.className = "author";
         
+        let title = document.createElement('p');
+        title.textContent = myLibrary[i].title;
+        title.className = "title";
+
+        let pages = document.createElement('p');
+        pages.textContent = `${myLibrary[i].pages} p.`;
+        pages.className = "pages";
+
+        let body = document.querySelector('body');
+        let cardDiv = document.createElement('div')
+        cardDiv.className = "card";
+        let textAreaDiv = document.createElement('div')
+        textAreaDiv.className = "text-area";
+        let stampsAreaDiv = document.createElement('div')
+        stampsAreaDiv.className = "stamps-area";
+        let subFieldDiv = document.createElement('div')
+        subFieldDiv.className = "sub-field";
+        let dateDiv = document.createElement('div')
+        dateDiv.className = "date";
+        dateDiv.textContent = myLibrary[i].dateEntered();
+        let readDiv = document.createElement('div')
+        readDiv.className = "read";
+
+        let imageElement = document.createElement('img')
+        imageElement.className = "check";
+        console.log(imageElement);
+        if (myLibrary[i].isRead === false) {
+            imageElement.setAttribute('src', "images/unchecked.png");
+        } else {
+            imageElement.setAttribute('src', "images/check.png");
+        }   
+        
+        // throw it all together
+        body.appendChild(cardDiv);
+        cardDiv.appendChild(textAreaDiv);
+        cardDiv.appendChild(stampsAreaDiv);
+        textAreaDiv.appendChild(author);
+        textAreaDiv.appendChild(subFieldDiv);
+        subFieldDiv.appendChild(title);
+        subFieldDiv.appendChild(pages);
+        stampsAreaDiv.appendChild(dateDiv);
+        stampsAreaDiv.appendChild(readDiv);
+        readDiv.appendChild(imageElement);
     }
 }
 
 //addBookToLibrary();
-//displayLibrary();
+displayLibrary();
+
+
+
+
 
 
 const checkButtons = document.querySelectorAll('.check');
